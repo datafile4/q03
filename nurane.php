519372f9cd28945378b06282d228e3b5c899a1a6
Emil Alasgarov <debianmik@gmail.com>
2015-08-30 10:00:34 +0500

    added function for crate tables

--- nurane.php
+++ nurane.php
@@ -1,3 +1,18 @@
-<?php
- sadddwd
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.css" style="text/css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+  </head>
+  <body>
+    <table class="table table-striped">
+      <?php
+        $myProjects = array(0=>array('name'=>'emil','surname'=>'Alasgarov'),1=>array('name'=>'vasya','surname'=>'pupkin'));
+        include('functions.inc');
+        createTable($myProjects);
+       ?>
+    </table>
+  </body>
+</html>
