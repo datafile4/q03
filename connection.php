ab0b18f7b39508c79ecf66e5283176012e581179
Emil Alasgarov <debianmik@gmail.com>
2015-09-15 20:17:21 +0500

    added adminlogin.php

--- connection.php
+++ connection.php
@@ -1,5 +1,11 @@
-<?php 
-
-$dbconnect = mysqli_connect('localhost', 'root', 'root21','freelance');
+<?php
+  //page for connect to database
+  //data from admin.html
+  $host = $_POST['server'];
+  $username = $POST['pass'];
+  $pass = $POST['pass'];
+  $dbname = $POST['dbname'];
+  //connection to db
+  $dbconnect = mysqli_connect($host, $username, $pass,$dbname);
 
 ?>
