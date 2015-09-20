199745b04fd873052857c316c81c1fdacf84a1ff
Emil Alasgarov <debianmik@gmail.com>
2015-09-20 22:00:14 +0500

    reverted to stable

--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,9 @@
 	<?php
 		//	checking user login
 			include("session.php");
-			echo $user_session_status;
-			if($user_session_status){
+
 				include 'templates/myAccount.php';
 				include 'templates/navbar.php';
 				include 'templates/dashboard.html';
-			} else {
-				echo "access denied";
-			}
 
 		?>
